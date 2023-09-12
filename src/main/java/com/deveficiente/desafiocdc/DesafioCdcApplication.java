8cc63d8816843ac5a05ffa57f8eb50b82f34eed2
vsantsal <vsantsal@gmail.com>
2023-09-11 23:06:07 -0300

    config: iniciando aplicacao spring boot

---
+++ src/main/java/com/deveficiente/desafiocdc/DesafioCdcApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiocdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCdcApplication.class, args);
+	}
+
+}
