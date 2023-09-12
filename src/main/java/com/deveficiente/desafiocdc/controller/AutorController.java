511220b6d036dd3d6c414d1849079f143273450f
vsantsal <vsantsal@gmail.com>
2023-09-11 23:20:55 -0300

    feat: repository e controller para cadastro de autor

---
+++ src/main/java/com/deveficiente/desafiocdc/controller/AutorController.java
@@ -0,0 +1,11 @@
+package com.deveficiente.desafiocdc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cdc/autor")
+public class AutorController {
+
+
+}
