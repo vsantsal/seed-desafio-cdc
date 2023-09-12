8bd316090fa4f2d98e214f9b5f0891a9191caf2c
vsantsal <vsantsal@gmail.com>
2023-09-11 23:18:04 -0300

    feat: entidade do dominio

--- src/main/java/com/deveficiente/desafiocdc/domain/entity/Autor.java
+++ src/main/java/com/deveficiente/desafiocdc/domain/entity/Autor.java
@@ -1,4 +1,48 @@
 package com.deveficiente.desafiocdc.domain.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 400)
+    private String descricao;
+
+    @Column(name = "data_criacao", nullable = false)
+    private LocalDateTime criadoEm;
+
+    @Deprecated
+    public Autor(){
+
+    }
+
+    public Autor(@NotBlank String nome, @NotBlank @Email String email, @NotBlank String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+        this.criadoEm = LocalDateTime.now();
+    }
+
+
 }
